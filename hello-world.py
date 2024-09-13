f7ed1bc9f834509f0f4fa8459ba2c61c0839d75c
compese <128396302+compese@users.noreply.github.com>
2024-09-13 12:07:34 -0600

    add name param to hello_world()

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,4 @@
 from funcs import hello_world
 
 if __name__ == "__main__":
-    print(hello_world())
+    print(hello_world('folks'))
--- test_hello_world.py
+++ test_hello_world.py
@@ -3,7 +3,7 @@ from funcs import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world_output(self):
-        self.assertEqual(hello_world(), "Hello, World!")
+        self.assertEqual(hello_world('folks'), "Hello, folks!")
 
 if __name__ == "__main__":
     unittest.main()
